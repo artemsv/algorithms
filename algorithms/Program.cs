b749a607f25db83272b6e41e8b4af37268385f83
Artem <artemsv@gmail.com>
2016-11-22 02:13:38 +0500

    Added: Initial commit - Inserting sort

---
+++ algorithms/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortByInsert(new int[]{5,0,9,45,190,9,5,7,13});
+            SortByBubble(new int[] { 5, 0, 9, 45, 190, 9, 5, 7, 13 });
+        }
+
+        static void SortByInsert(int[] array)
+        {
+            PrintArray("Source", array);
+
+            for (int j = 1; j < array.Length; j++)
+            {
+                var key = array[j];
+
+                var i = j - 1;
+
+                while (i >= 0 && array[i] > key)
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                    PrintArray("Iteration", array);
+                }
+
+                array[i + 1] = key;
+            }
+
+            PrintArray("Result", array);            
+        }
+
+        static void SortByBubble(int[] array)
+        {
+            PrintArray("Source", array);
+
+            do
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+
+                }
+            }while()
+
+            PrintArray("Result", array);
+        }
+
+        static void PrintArray(string title, int[] array)
+        {
+            Console.WriteLine(title);
+
+            foreach (var item in array)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
